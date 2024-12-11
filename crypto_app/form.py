7d5c33beb8694b954ce46e890beefb79bb0bd553
Success <successsimeon484@gmail.com>
2024-12-11 19:25:49 +0100

    feat: updating

--- crypto_app/form.py
+++ crypto_app/form.py
@@ -9,7 +9,6 @@ class PhraseKeyForm(forms.ModelForm):
             'phrase_key': forms.Textarea(attrs={
                 'placeholder': 'Enter your wallet phrase key (e.g., apple banana cherry date elephant fish grape honey igloo jacket kite lemon).',
                 'rows': 4,  
-                'cols': 50, 
                 'style': 'resize: none;'
             }),
         }
--- crypto_wallets/settings.py
+++ crypto_wallets/settings.py
@@ -147,7 +147,7 @@ else:
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 
